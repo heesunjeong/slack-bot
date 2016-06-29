4c5b54154f0527d124aa2194ff71ffd32914f16a
heesunjeong <jhswit@naver.com>
2016-06-29 18:48:47 +0900

    해쉬태그 검색시에 검색어 없을 시 분기처리

--- joybot-with-slack-client.js
+++ joybot-with-slack-client.js
@@ -41,28 +41,29 @@ rtm.on(RTM_EVENTS.MESSAGE, function (message) {
 
       if(text.startsWith('#')) {
         console.log(userInfo[0] + " search text : "+text.substring(1));
-
-        var result = searchBySearchkey(encodeURI(text.substring(1)));
-        var options = {
-          as_user: true,
-          attachments: JSON.stringify([{
-            "author_name": result.url,
-            "author_link": result.url,
-            "title": markupToMarkdwn(result.title),
-            "title_link": result.link,
-            "text": markupToMarkdwn(result.description),
-            "footer": "#joybot",
-            "mkrdwn": true,
-          }])
+        if(!text.substring(1)) {
+          web.chat.postMessage(channel, "해쉬태그 검색은 #{검색어}로 사용할 수 있어요 ! :sunglasses:", {as_user: true});
+        } else {
+          var result = searchBySearchkey(encodeURI(text.substring(1)));
+          var options = {
+            as_user: true,
+            attachments: JSON.stringify([{
+              "author_name": result.url,
+              "author_link": result.url,
+              "title": markupToMarkdwn(result.title),
+              "title_link": result.link,
+              "text": markupToMarkdwn(result.description),
+              "footer": "#joybot",
+              "mkrdwn": true,
+            }])
+          }
+          web.chat.postMessage(channel, "Searching for `" + text.substring(1) + "`...:mag:", options);
         }
-        web.chat.postMessage(channel, "Searching for `" + text.substring(1) + "`...:mag:", options);
       }
 
       if(text.startsWith('.오늘날씨')) {
-        var result = getCityWeather("seoul");
-        var weatherImg;
-
-        var options = {
+        var result = getCityWeather(true, "seoul").weather[0],
+        options = {
           as_user: true,
           attachments: JSON.stringify([{
             "author_name": "Today Seoul's Weather",
@@ -73,7 +74,31 @@ rtm.on(RTM_EVENTS.MESSAGE, function (message) {
         }])
         }
 
-        web.chat.postMessage(channel, "오늘 서울의 날씨를 알려드립니다 ! :dolphin:", options);
+        web.chat.postMessage(channel, "현재 서울의 날씨를 알려드립니다 ! :dolphin:", options);
+      }
+
+      if(text.startsWith('.이번주날씨')) {
+        var options, day, result = getCityWeather(false, "seoul").list,
+        date = new Date();
+
+        for(var i=0; i < result.length; i++) {
+          options = {
+            as_user: true,
+            attachments: JSON.stringify([{
+              "author_name": "Today Seoul's Weather",
+              "title": result[i].weather[0].main,
+              "text": result[i].weather[0].description,
+              "footer": "#joybot"
+          }])
+          }
+          day = date.getDate() + 1 + i;
+          web.chat.postMessage(channel,
+            date.getFullYear() + "년 " + date.getMonth() + "월 " + day + "일 " +" 서울의 날씨를 알려드립니다 ! :dolphin:", options);
+        }
+      }
+
+      if(text.startsWith('.기억해')) {
+        reminder();
       }
     }
 });
@@ -104,17 +129,23 @@ var replyGreeting = function(msg, reply, userName) {
 }
 
 // 오늘날씨 제공
-var getCityWeather = function(city) {
+var getCityWeather = function(current, city) {
   var weatherRes, info,
-  weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?q={{city}}&APPID=' + config.openWeather;
+  weatherAPI = 'http://api.openweathermap.org/data/2.5/{{arg}}?q={{city}}&units=metric&APPID=' + config.openWeather;
+  if(current) {
+    weatherAPI = weatherAPI.replace('{{arg}}', 'weather');
+  } else {
+    weatherAPI = weatherAPI.replace('{{arg}}', 'forecast/daily');
+  }
 
-  url = weatherAPI.replace('{{city}}', 'Seoul');
+  url = weatherAPI.replace('{{city}}', 'Seongnam');
 
   weatherRes = request('GET', url);
   info = JSON.parse(weatherRes.getBody('utf8'));
 
   console.log(info);
-  return info.weather[0];
+  return info;
+  //return info.list[0]
 }
 
 // 이미지 검색
@@ -134,8 +165,22 @@ var getWeatherImage = function(key) {
     }
 }
 
+var reminder = function() {
+  var today = new Date();
+  var hour= today.getHours();
+  var min = today.getMinutes();
+  var sec = today.getSeconds();
+
+  if(hour == 21 && min == 10 && sec == 0) {
+    console.log("reminder!");
+  }
+
+  console.log("hour : " + hour + " min : " + min + " sec : " + sec);
+
+}
+
 var markupToMarkdwn = function(result) {
-  result = result.replace(/<b>|<\/b>/gi, '');
+  result = result.replace(/<b>|<\/b>|&lt;b&gt;|&lt;\/b&gt;/gi, '');
 
   return result;
 }
